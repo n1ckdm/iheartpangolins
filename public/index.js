1a2fb59002402d0c4da0cda7be11e6230175b072
Nick M <nicholas.martin@tharsus.co.uk>
2020-01-29 18:07:47 +0000

    not finished yet >.<

---
+++ public/index.js
@@ -0,0 +1,17 @@
+
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+readTextFile("particlesjs-config.json", function(text){
+    particlesJS("particles-js", JSON.parse(text));
+});
+
